41c6ea7516365a33e9cdfef04f4a70d1468f8aff
MattJurkowski <jurek198@gmail.com>
2024-04-06 10:53:44 +0000

    PROGRAM AFTER CHANGES
    Signed-off-by: MattJurkowski <jurek198@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,19 +4,30 @@ int n_1 = 0;
 int n_2 = 1;
 int n_3 = 0;
 
-int max_value = 10;
+int max_value =0 ;
+
+int fib(int n){
+    if(n ==0 || n == 1){
+        printf("*");
+        return n;
+    }
+    else{
+        printf("(%d), (%d)", n-1, n-2);
+        return (fib(n-1) + fib(n-2));
+    }
+
+}
+
 
 // main() runs in its own thread in the OS
 int main()
 {
     printf("lab 2 start!!!\r\n");
-
-    printf("series is: %d,%d, ", n_1, n_2);
-    for(int i=3; i<max_value; i++){
-         n_3 = n_1 + n_2;
-         printf("%d, ",n_3);
-         n_1 = n_2;
-         n_2 = n_3;
+    printf("series is: ");
+    for (int i=0; i<max_value; i++){
+        printf("%d, ", fib(i));
+    }
+    printf("r\n");
 
 while (true) {
 
