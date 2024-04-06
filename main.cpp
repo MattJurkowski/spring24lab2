bf6ac8d5bf276b127120e9856592b61885032748
MattJurkowski <jurek198@gmail.com>
2024-04-06 10:36:31 +0000

    commit 1
    Signed-off-by: MattJurkowski <jurek198@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include "mbed.h"
 
+int n_1 = 0;
+int n_2 = 1;
+int n_3 = 0;
+
+int max_value = 10;
+
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
+    printf("lab 2 start!!!\r\n");
+
+    printf("series is: %d,%d, ", n_1, n_2);
+    for(int i=3; i<max_value; i++){
+         n_3 = n_1 + n_2;
+         printf("%d, ",n_3);
+         n_1 = n_2;
+         n_2 = n_3;
+
+while (true) {
 
     }
 }
